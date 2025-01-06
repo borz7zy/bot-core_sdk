cd34dc785898fa4f7b9dcafce4840881046b3faf
borz7zy <borz7zy@yandex.ru>
2025-01-07 00:20:30 +0100

    update sdk for new plugin system (coderef-1)

--- sdk.hxx
+++ sdk.hxx
@@ -1,32 +1,26 @@
-#ifndef _SDK_HXX
-#define _SDK_HXX
-
-#if defined(_MSC_VER)
-#define PACKED
-#else
-#define PACKED __attribute__((packed))
-#endif
+#ifndef _PLUGINS_SDK_HXX
+#define _PLUGINS_SDK_HXX
 
 extern "C"
 {
-#include "lauxlib.h"
 #include "lua.h"
-#include "luaconf.h"
-#include "lualib.h"
 }
 
-typedef int (*IntFunctionPtr)(lua_State *L);
-
-typedef struct myNative_Function_List
-{
-    const char *nativeName PACKED;
-    IntFunctionPtr funcPtr PACKED;
-} PACKED Native_Function_List;
-
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
 #define PLUGIN_EXPORT __declspec(dllexport)
 #else
-#define PLUGIN_EXPORT
+#define PLUGIN_EXPORT __attribute__((visibility("default")))
 #endif
+
+class SDKBase
+{
+public:
+    virtual ~SDKBase() = default;
+    virtual void update() = 0;
+    virtual void register_lua_natives(lua_State *L) = 0;
+    virtual void onRun() = 0;
+};
+
+#define PLUGIN_ENTRY extern "C" PLUGIN_EXPORT SDKBase *PluginEntryGet
 
 #endif
